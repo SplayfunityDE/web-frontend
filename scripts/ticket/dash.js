be1e4554df376a28cb7d31fd39cefafacfd39933
splayfery <simon.liedlc@gmail.com>
2025-07-14 16:22:51 +0200

    more dashboard charts

--- scripts/ticket/dash.js
+++ scripts/ticket/dash.js
@@ -13,9 +13,9 @@
             data: [300, 50, 100],
             borderAlign: "inner",
             backgroundColor: [
-            'rgb(95, 125, 140, 1)',
-            'rgb(255, 205, 86)',
-            'rgb(227, 100, 100)'
+            'rgb(141, 171, 186)',
+            'rgb(250, 211, 117)',
+            'rgb(235, 138, 138)'
             ],
             hoverOffset: 4
         }]
@@ -36,18 +36,76 @@ new Chart(bearbeiter, {
 type: 'doughnut',
 data: {
     labels: [
-        'Allgemein',
-        'Bug'
+        'In Bearbeitung',
+        'Ausstehend'
     ],
     datasets: [{
         label: 'My First Dataset',
-        data: [300, 50],
+        data: [4, 8],
         borderAlign: "inner",
         backgroundColor: [
-        'rgb(52,179,228,255)',
-        'rgb(155,218,243,255)'
+        '#4b7a7e',
+        '#74C2C4'
         ],
         hoverOffset: 4
+    }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: "bottom"
+            }
+        }
+    }
+});
+
+const irgendwas = document.getElementById('irgendwasChart');
+
+new Chart(irgendwas, {
+type: 'doughnut',
+data: {
+    labels: [
+        'In asdfasdf',
+        'Ausasfddasfstehend'
+    ],
+    datasets: [{
+        label: 'My First Dataset',
+        data: [4, 3, 8],
+        borderAlign: "inner",
+        backgroundColor: [
+        'rgb(175, 122, 197)',
+        'rgb(195, 155, 211)',
+        'rgb(227, 202, 237)'
+        ],
+        hoverOffset: 4
+    }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: "bottom"
+            }
+        }
+    }
+});
+
+const activity = document.getElementById('activityChart');
+
+new Chart(activity, {
+type: 'line',
+data: {
+    labels: [
+        'test1',
+        'test2'
+    ],
+    datasets: [{
+        label: 'My First Dataset',
+        data: [65, 59],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
     }]
     },
     options: {
